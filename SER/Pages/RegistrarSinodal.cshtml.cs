369ffabca47ed07c2457ee8c5a4ba5de876c4b6f
OMAR BALTAZAR ISLAS <zS18012180@estudiantes.uv.mx>
2022-02-02 21:04:04 -0600

    validation fix

--- SER/Pages/RegistrarSinodal.cshtml.cs
+++ SER/Pages/RegistrarSinodal.cshtml.cs
@@ -6,11 +6,15 @@ namespace SER.Pages
 {
     public class RegistrarSinodalModel : PageModel
     {
-        //private SERContext DbContext { get; set; }
+        private readonly SERContext _context;
 
         [BindProperty]
         public SinodalDelTrabajo SinodalDelTrabajo { get; set; }
 
+        public RegistrarSinodalModel(SERContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
         }
@@ -19,13 +23,19 @@ namespace SER.Pages
         {
             if (ModelState.IsValid)
             {
-                using (var DbContext = new SERContext())
+                var sinodalesExistentes = _context.SinodalDelTrabajos.ToList();
+                bool sinodalYaExiste = sinodalesExistentes.Any(s => s.CorreoElectronico.Equals(SinodalDelTrabajo.CorreoElectronico));
+                if (sinodalYaExiste)
                 {
-                    DbContext.SinodalDelTrabajos.Add(SinodalDelTrabajo);
-                    DbContext.SaveChanges();
-                    new OkObjectResult(SinodalDelTrabajo.Nombre);
+                    return Page();
                 }
-                return RedirectToPage("/Menus/UIMaestro");
+                else
+                {
+                    _context.SinodalDelTrabajos.Add(SinodalDelTrabajo);
+                    _context.SaveChanges();
+                    new OkObjectResult(SinodalDelTrabajo.Nombre);
+                    return RedirectToPage("/Menus/UIMaestro");
+                }                              
             }
             return Page();
         }
